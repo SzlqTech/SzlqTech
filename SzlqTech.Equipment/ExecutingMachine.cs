765cdf346a0c0bc543353c1c718a59f9456ef06b
ningyilang <1982849947@qq.com>
2025-05-30 09:09:17 +0800

    1,机器列表服务开发

--- SzlqTech.Equipment/ExecutingMachine.cs
+++ SzlqTech.Equipment/ExecutingMachine.cs
@@ -1,11 +1,44 @@
-﻿using SzlqTech.Common.Extensions;
+﻿using HslCommunication.Core.Net;
+using NLog;
+using SzlqTech.Common.Extensions;
 using SzlqTech.Equipment.Machine;
+using SzlqTech.IService;
 
 namespace SzlqTech.Equipment
 {
     public class ExecutingMachine : IExecutingMachine
     {
         public event EventHandler<TEventArgs<MachineData>>? DataReceived;
+
+        /// <summary>
+        /// 日志
+        /// </summary>
+        public static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// 机器列表
+        /// </summary>
+        private readonly IMachineSettingService machineSettingService;
+
+        /// <summary>
+        /// 机器明细详情
+        /// </summary>
+        private readonly IMachineDetailService machineDetailService;
+
+        /// <summary>
+        /// 网口TCP设备字典
+        /// </summary>
+        public Dictionary<string, NetworkDeviceBase> TcpDeviceDictionary { get; }= new Dictionary<string, NetworkDeviceBase>();
+
+        
+
+        public ExecutingMachine(IMachineSettingService machineSettingService,IMachineDetailService machineDetailService)
+        {
+            this.machineSettingService = machineSettingService;
+            this.machineDetailService = machineDetailService;
+        }
+
+
 
         public void CloseMachine()
         {
