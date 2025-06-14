09822da5c897473d4ed5ab12f571cbac0691f51d
ningyilang <1982849947@qq.com>
2025-06-14 15:35:44 +0800

    1.增加数据看板

--- SzlqTech.Core.WorkFlow/ViewModels/InnoLightChartViewModel.cs
+++ SzlqTech.Core.WorkFlow/ViewModels/InnoLightChartViewModel.cs
@@ -1,7 +1,13 @@
 ﻿
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
 using SzlqTech.Core.Consts;
 using SzlqTech.Core.ViewModels;
 using SzlqTech.Localization;
+
 
 namespace SzlqTech.Core.WorkFlow.ViewModels
 {
@@ -11,5 +17,37 @@ namespace SzlqTech.Core.WorkFlow.ViewModels
         {
             Title = LocalizationService.GetString(AppLocalizations.ChartView);
         }
+
+        public ISeries[] Series { get; set; } =
+    {
+        new ColumnSeries<double>
+        {
+            Name = "Mary",
+            Values = new double[] { 2, 5, 4 }
+        },
+        new ColumnSeries<double>
+        {
+            Name = "Ana",
+            Values = new double[] { 3, 1, 6 }
+        }
+    };
+
+        public Axis[] XAxes { get; set; } =
+        {
+        new Axis
+        {
+            Labels = new string[] { "Category 1", "Category 2", "Category 3" },
+            LabelsRotation = 0,
+            SeparatorsPaint = new SolidColorPaint(new SKColor(200, 200, 200)),
+            SeparatorsAtCenter = false,
+            TicksPaint = new SolidColorPaint(new SKColor(35, 35, 35)),
+            TicksAtCenter = true,
+            // By default the axis tries to optimize the number of 
+            // labels to fit the available space, 
+            // when you need to force the axis to show all the labels then you must: 
+            ForceStepToMin = true,
+            MinStep = 1
+        }
+    };
     }
 }
